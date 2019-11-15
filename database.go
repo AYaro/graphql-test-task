9ce3b4a43827248ec7f0f86dacec6ce0806d8c34
Alex Yaro <0370384@mail.ru>
2019-11-15 14:42:16 +0300

    changed mongodb network back to localhost

--- database.go
+++ database.go
@@ -28,7 +28,7 @@ func mustConnectToMongo() {
 
 func connectToMongo() error {
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://backend:27017"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
 	if err != nil {
 		return err
 	}
